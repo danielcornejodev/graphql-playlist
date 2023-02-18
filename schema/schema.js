20993adf653716998ad5f5567611b0df655701d4
Daniel Cornejo <contact@danielcornejo.dev>
2023-02-18 07:38:08 -0500

    GraphQL Tutorial #13 - Type Relations

--- schema/schema.js
+++ schema/schema.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 
 //destructuring grabs function GraphQLObjectType from graphql variable. 
 //this code grabs different properties from the graphql package.
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 //Dummy data
 var books = [
@@ -12,12 +12,27 @@ var books = [
     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ];
 
+var authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
+];
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        age: { type: GraphQLInt},
+        name: { type: GraphQLString }
     })
 });
 
@@ -34,6 +49,13 @@ const RootQuery = new GraphQLObjectType({
                 //using lodash to look through the books array and finding any book with the id that has been attached the args.id that user sends along.
                 //args to id is converted to a string tyoe. You can verify by console.logging typeof args.id
                 return _.find(books, {id: args.id})
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                return _.find(authors, { id: args.id });
             }
         }
     }
