31ea56ee4ea608c9210e9c73348fa931124df1b0
Daniel Cornejo <contact@danielcornejo.dev>
2023-02-20 07:18:06 -0500

    GraphQL Tutorial #23 - Create React App

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,7 @@ const Author = require('../models/author');
 
 //destructuring grabs function GraphQLObjectType from graphql variable. 
 //this code grabs different properties from the graphql package.
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList, GraphQLNonNull } = graphql;
 
 //Dummy data
 // var books = [
@@ -106,8 +106,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: { type: GraphQLString },
-                age: { type: GraphQLInt }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
             },
             resolve(parent, args){
                 let author = new Author({
@@ -120,9 +120,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: { type: GraphQLString },
-                genre: { type: GraphQLString },
-                authorId: {type: GraphQLID}
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: {type: new GraphQLNonNull(GraphQLID)}
             },
             resolve(parent, args){
                 let book = new Book({
