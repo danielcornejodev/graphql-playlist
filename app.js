77c191f931ac6b3a49b01332f2a37ec197a2dbf2
Daniel Cornejo <contact@danielcornejo.dev>
2023-02-25 10:35:12 -0500

    graphql-http instead of graphql-express

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+const { createHandler } = require('graphql-http/lib/use/express');
 const schema = require('./schema/schema');
-const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
 //express graphql is considered middleware.
 //use schema to map out the graph so you know what happens when user makes a request to this endpoint. 
+// mongoose.set("strictQuery", false);
 
 mongoose.connect('mongodb+srv://danielcornejodev:test123@cluster0.czuywlx.mongodb.net/?retryWrites=true&w=majority');
 mongoose.connection.once('open', () => {
     console.log('connected to database');
 })
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
+// app.all('/graphql', createHandler({
+//     schema,
+//     graphiql: true
+// }));
+
+app.all('/graphql', createHandler({ 
+    schema, 
+    graphiql: true 
 }));
 
-app.listen(5500, () => {
-    console.log('now listening for requests on port 5500');
+app.listen(5501, () => {
+    console.log('now listening for requests on port 5501');
 })
