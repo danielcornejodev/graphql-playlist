a7476b80d0e2b28fac5037d5f594e61bb6984660
Daniel Cornejo <contact@danielcornejo.dev>
2023-02-25 19:16:27 -0500

    create Book list component and render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Booklist from './components/BookList';
 
 export default function App() {
   return (
-    <div className="App">
-
+    <div id="name">
+      <h1>Reading List</h1>
+      <Booklist />
     </div>
   );
 }
---
+++ src/components/BookList.js
@@ -0,0 +1,10 @@
+
+export default function Booklist() {
+    return (
+      <div id="name">
+        <ul id="book-list">
+            <li>Book name</li>
+        </ul>
+      </div>
+    );
+  }
