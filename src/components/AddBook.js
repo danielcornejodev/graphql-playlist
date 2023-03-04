429acf29e8f7762594703e6ad2a2418913153754
Daniel Cornejo <contact@danielcornejo.dev>
2023-03-04 11:01:31 -0500

    use state onChange

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,9 +1,13 @@
 import { useQuery } from '@apollo/client';
 import { GET_AUTHORS } from '../queries/queries';
+import { useState } from 'react';
 
 
-export default function AddBook({ onAuthorSelected }) {
+export default function AddBook() {
     const { loading, error, data } = useQuery(GET_AUTHORS);
+    const [Name, setName] = useState("");
+    const [genre, setGenre] = useState("");
+    const [authorId, setAuthorId] = useState("");
   
     if (loading) return 'Loading...';
     if (error) return `Error! ${error.message}`;
@@ -14,17 +18,17 @@ export default function AddBook({ onAuthorSelected }) {
           <label>
             Book name:
           </label>
-          <input type="text" />
+          <input type="text" onChange={(e) => setName(e.target.value)}/>
         </div>
         <div className="field">
           <label>
             Genre:
           </label>
-          <input type="text" />
+          <input type="text" onChange={(e) => setGenre(e.target.value)}/>
         </div>
         <div>
           <label>Author:</label>
-          <select name='author' onChange={onAuthorSelected}>
+          <select name='author' onChange={(e) => setAuthorId(e.target.value)}>
           <option>Select Author</option>
           {data.authors.map((author) => (
               <option key={author.id} value={author.name}>
@@ -33,6 +37,7 @@ export default function AddBook({ onAuthorSelected }) {
             ))}
           </select>
         </div>
+        <button>+</button>
       </form>  
       </>
     );
